2b90358f4e36d2b94e6cc477672706cf6d4d97de
Max Kleinhenz <m.kleinhenz@novum-engineering.com>
2025-09-05 21:50:50 +0200

    init

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+
+	"chore-manager/views"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", homeHandler)
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func homeHandler(c echo.Context) error {
+
+	titlePage := "Test"
+
+	return renderView(c, views.Home(
+		titlePage,
+	))
+}
+
+func renderView(c echo.Context, cmp templ.Component) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return cmp.Render(c.Request().Context(), c.Response().Writer)
+}
